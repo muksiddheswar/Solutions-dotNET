5061f719eddcce9d396230722b16a993eb6a941c
konkeror <sid.mkjee@gmail.com>
2025-12-23 18:16:14 +0100

    Completed exercise LuciansLusciousLasagna

---
+++ Exercism/csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,23 @@
+class Lasagna
+{
+    public int ExpectedMinutesInOven()
+        {
+        return 40;
+    }
+
+    public int RemainingMinutesInOven(int actualMinutesInOven)
+    {
+        return ExpectedMinutesInOven() - actualMinutesInOven;
+    }
+
+    public int PreparationTimeInMinutes(int numberOfLayers)
+    {
+        return numberOfLayers * 2;
+    }
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven)
+    {
+        return PreparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+    }
+}
